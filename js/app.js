b3a4a2971ddf2319c7826449a3893bd48ea6d500
horaciod12 <daddarioh@gmail.com>
2017-06-01 13:01:35 -0300

    design + get buttons value

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 // A module pattern should be used here
+
+var myBasicCalculator = (function() {
+
+
+
+$('.buttons, .cero').on('click', function(){
+	
+	var value = $(this).val();
+	var span = document.createElement('span');
+	span.append(value);
+	$('#display').append(span);
+
+});
+
+
+
+
+})();
